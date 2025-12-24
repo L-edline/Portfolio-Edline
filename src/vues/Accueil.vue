be783996e298385874535a10e068d747092a06a5
Louis Edline <louis.edline.pro@gmail.com>
2025-12-24 17:13:22 +0100

    frontend +responsive

--- src/vues/Accueil.vue
+++ src/vues/Accueil.vue
@@ -28,26 +28,25 @@
                 ma formation ou de mon travail, et ceux réalisés ailleurs.
             </p>
 
+            <div class="CV">
+                <router-link to="/CV" class="routeur">
+                    <h2>Lien vers le CV</h2>
+                </router-link>
+            </div>
             
-        </div>
-
-        <div class="CV">
-            <router-link to="/CV" class="routeur">
-                <h2>Mon CV</h2>
-            </router-link>
         </div>
 
         <div class="wrapperCenter">
             <div class="typeProjet">
-                <div class="tileType" @click="typeProjet = 'formation'" style="cursor: pointer">
+                <div class="tileType" @click="typeProjet = 'formation'" style="cursor: pointer" role="button" tabindex="-1" >
                     <p class="legende">Projets en formation</p>
                 </div>
 
-                <div class="tileType" @click="typeProjet = 'perso'" style="cursor: pointer">
+                <div class="tileType" @click="typeProjet = 'perso'" style="cursor: pointer" role="button" tabindex="-1">
                     <p class="legende">Projets personnels</p> 
                 </div>
 
-                <div class="tileType" @click="typeProjet = 'entreprise'" style="cursor: pointer">
+                <div class="tileType" @click="typeProjet = 'entreprise'" style="cursor: pointer" role="button" tabindex="-1">
                     <p class="legende">Projets en entreprise</p> 
                 </div>
             </div>
@@ -56,23 +55,23 @@
         <div class="projets">
             <div class="wrapperCenter">
                 <div v-if="typeProjet === 'formation'" class="listeProjets formation">
-                    <div class="tile" @click="projet = 'Nolark'" style="cursor: pointer">
+                    <div class="tile" @click="projet = 'Nolark'" style="cursor: pointer" role="button" tabindex="-1">
                         <p class="legende">Site de Nolark</p>
                     </div>
 
-                    <div class="tile" @click="projet = 'GSB'" style="cursor: pointer">
+                    <div class="tile" @click="projet = 'GSB'" style="cursor: pointer" role="button" tabindex="-1">
                         <p class="legende">Application GSB</p> 
                     </div>
                 </div>
 
                 <div v-if="typeProjet === 'entreprise'" class="listeProjets entreprise">
-                    <div class="tile" @click="projet = 'CDC'" style="cursor: pointer">
+                    <div class="tile" @click="projet = 'CDC'" style="cursor: pointer" role="button" tabindex="-1">
                         <p class="legende">Formulaire de CDC</p>
                     </div>
                 </div>
 
                 <div v-if="typeProjet === 'perso'" class="listeProjets perso">
-                    <div class="tile" @click="projet = 'pokedex'" style="cursor: pointer">
+                    <div class="tile" @click="projet = 'pokedex'" style="cursor: pointer" role="button" tabindex="-1">
                         <p class="legende">Pokedex depuis API</p>
                     </div>
                 </div>
@@ -164,6 +163,14 @@
         color: black;
     }
 
+    .tileType:hover{
+        background-color: rgb(172, 165, 165); 
+    }
+
+    .tileType:focus{
+        background-color: rgb(141, 251, 141);
+    }
+
     .tile {
         margin-right: 2cap;
         margin-left: 2cap;
@@ -173,6 +180,14 @@
         border-radius: 0.5cap;
         background-color: rgb(76, 189, 76);
         color: black;
+    }
+
+    .tile:hover {
+        background-color: rgb(58, 130, 58);
+    }
+
+    .tile:focus{
+        background-color: rgb(58, 130, 58);
     }
 
     .legende {
@@ -190,10 +205,39 @@
     .CV {
         display: flex;
         justify-content: center;
+        margin-left: auto;
+        margin-right: 35cap;
+        background-color: wheat;
+        padding-left: 1cap;
+        padding-right: 1cap;
+        border-style: solid;
+        border-color: black;
+        border-radius: 1cap;
     }
 
     .routeur {
-        color: green;
+        color: rgb(0, 0, 0);
+        text-decoration: none;
+    }
+
+    @media (max-width: 600px) {
+        .presentation{
+            flex-direction: column;
+        }
+
+        .texteLinkedin {
+            margin: 0px;
+        }
+
+        .CV {
+            margin-left: 0px;
+            margin-right: 0px;
+            margin-top: 2cap;
+        }
+
+        h1 {
+            text-align: center;
+        }
     }
 
 </style>
--- src/vues/CV.vue
+++ src/vues/CV.vue
@@ -7,11 +7,13 @@
 
 <template>
     <div class="page">
-        <h1>Mon CV</h1>
-    
-        <router-link to="/" class="routeur">
-            <h2>Retour au portfolio</h2>
-        </router-link>
+        <h1>CV de Louis EDLINE</h1>
+        
+        <div class="lienretour">
+            <router-link to="/" class="routeur">
+                <h2>Retour au portfolio</h2>
+            </router-link>
+        </div>
         
         <div class="cv">
             <VuePDF :pdf="pdf" />
@@ -31,9 +33,25 @@
     .cv {
         width: 100%;
         overflow: auto;
+        display: flex;
+        justify-content: center;
     }
 
     .routeur {
-        color: green;
+        color: rgb(0, 0, 0);
+        text-decoration: none;
     }
+
+    .lienretour {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 2cap;
+        background-color: wheat;
+        padding-left: 1cap;
+        padding-right: 1cap;
+        border-style: solid;
+        border-color: black;
+        border-radius: 1cap;
+    }
+
 </style>
